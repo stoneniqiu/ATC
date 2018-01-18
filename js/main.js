66fda29889b737adb9b3a45d785c9d3c1e778b1a
stoneniqiu <stonzrj@163.com>
2018-01-18 17:24:36 +0800

    随机增加飞机
    飞机品质还有待提高

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var ctx,width=1200,height=600;
 var airspace = [];
+var max=10;
 //38 40 37 39 上下左右
 var headings={38:"up",40:"down",37:"left",39:"right"};
 
@@ -7,10 +8,11 @@ ctx=document.getElementById("surface");
 
 var taget=0;
 
-function Plane(sx,sy,heading){
+function Plane(sx,sy,heading,url){
     this.x=sx;
     this.y=sy;
     this.h=heading||"down";//up down left right
+    this.img=url||"down.png";
     this.draw=drawPlane
     this.move=movePlane
 }
@@ -25,7 +27,10 @@ function drawPlane(){
  }
 
  var img=new Image();
- img.src="down.png";
+ img.src=this.img;
+ img.onclick=function(e){
+     console.log(e)
+ }
  ctx.drawImage(img,this.x,this.y,40,40);
  if(this.degree){
     ctx.restore();
@@ -41,12 +46,8 @@ function movePlane(dx,dy){
 var moving;
 function start(){
     ctx=document.getElementById("surface").getContext('2d');
-    //添加飞机
-    for(var i=0;i<2;i++){
-        var p=new Plane((i+1)*50,0);
-        airspace.push(p);
-    }
-    moving= setInterval(refrash,500);
+    moving= setInterval(refrash,200);
+    tower();
 }
 
 window.onload=start;
@@ -57,15 +58,19 @@ function refrash(){
         var hit=airspace[i];
         switch(hit.h){
             case "up":
+            hit.img="plane.png";
             hit.move(0,-2);
             break;
             case "down":
+            hit.img="down.png";
             hit.move(0,2)
             break;
             case "left":
+            hit.img="left.png";
             hit.move(-2,0)
             break;
             case "right":
+            hit.img="right.png";
             hit.move(2,0)
             break;
         }
@@ -87,6 +92,63 @@ window.onkeyup=function(e){
   console.log(e.keyCode,headings[e.keyCode]);
   headings[e.keyCode]&&(airspace[taget].h=headings[e.keyCode]);
   //38 40 37 39 上下左右
+}
+
+function getRandom(max){
+    var ran=Math.round(max*Math.random());
+   return ran>=max?getRandom(max):ran;
+}
+var headinglist={0:"up",1:"down",2:"left",3:"right"}//上下左右
+var imglist={0:"plane.png",1:"down.png",2:"left.png",3:"right.png"}
+
+ 
+
+function tower(){
+    
+    getRandomPlane();
+    getRandomPlane();
+
+   setInterval(function(){
+    if(airspace.length<max-2){
+        getRandomPlane();
+        getRandomPlane();
+    }
+   },5000)
+}
+
+
+function getRandomPlane(){
+    //方向
+    var heading=getRandom(4);
+    var x,y,h;
+    h=headinglist[heading];
+    var img=imglist[heading];
+    var times=100;
+    var min=20;
+    switch (heading){
+        case 0://上
+        y=height-100;
+        x=getRandom(10)*times;
+        break;
+        case 1://下
+        y=min;
+        x=getRandom(10)*times;
+        break;
+        case 2://左
+        x=width-300;;
+        y=getRandom(6)*times;
+        break;
+        case 3://右
+        x= min;
+        y=getRandom(6)*times;
+        break;
+    }
+
+    //还要检查飞机
+    var plane=new Plane(x,y,h,img);
+    console.log(x,y,plane);
+    airspace.push(plane);
+    //位置
 }
 
 
