7484531ee83eb954b3db5d7689020f79ac86386e
stoneniqiu <stonzrj@163.com>
2018-01-18 12:34:55 +0800

    飞机控制
    旋转移动还没解决

--- js/main.js
+++ js/main.js
@@ -1,24 +1,36 @@
-var ctx;
+var ctx,width=1200,height=600;
 var airspace = [];
+//38 40 37 39 上下左右
+var headings={38:"up",40:"down",37:"left",39:"right"};
 
 ctx=document.getElementById("surface");
 
-function Plane(sx,sy){
+var taget=0;
+
+function Plane(sx,sy,heading){
     this.x=sx;
     this.y=sy;
+    this.h=heading||"down";//up down left right
     this.draw=drawPlane
     this.move=movePlane
 }
 
 function drawPlane(){
- ctx.beginPath();
  //ctx.arc(this.x, this.y, 10, 0, Math.PI * 2, true);
+ ctx.save();
+
+ if(this.degree){
+    ctx.translate(this.x,this.y);
+    ctx.rotate(this.degree * Math.PI / 180);
+ }
+
  var img=new Image();
  img.src="down.png";
  ctx.drawImage(img,this.x,this.y,40,40);
- //ctx.closePath();
- //ctx.fillStyle = "blue";
- //ctx.fill();
+ if(this.degree){
+    ctx.restore();
+    ctx.translate(-this.x,-this.y);
+ }
 }
 
 function movePlane(dx,dy){
@@ -26,24 +38,56 @@ function movePlane(dx,dy){
     this.y+=dy;
 }
 
+var moving;
 function start(){
     ctx=document.getElementById("surface").getContext('2d');
     //添加飞机
-    for(var i=0;i<5;i++){
+    for(var i=0;i<2;i++){
         var p=new Plane((i+1)*50,0);
         airspace.push(p);
     }
-    setInterval(refrash,500);
+    moving= setInterval(refrash,500);
 }
 
-setTimeout(start,1000)
+window.onload=start;
 
 function refrash(){
-    ctx.clearRect(0,0, 1200, 600);
+    ctx.clearRect(0,0, width, height);
     for (var i = 0; i < airspace.length; i++) {
-        airspace[i].move(1, 5);
-        airspace[i].draw();
-        ctx.restore();
+        var hit=airspace[i];
+        switch(hit.h){
+            case "up":
+            hit.move(0,-2);
+            break;
+            case "down":
+            hit.move(0,2)
+            break;
+            case "left":
+            hit.move(-2,0)
+            break;
+            case "right":
+            hit.move(2,0)
+            break;
+        }
+        hit.draw();
       }
 }
 
+function rotate(n,deg){
+    var plane=airspace[n];
+    plane.degree=deg
+    console.log(plane.x,plane.y)
+}
+
+function stop(){
+    clearInterval(moving);
+}
+
+window.onkeyup=function(e){
+  console.log(e.keyCode,headings[e.keyCode]);
+  headings[e.keyCode]&&(airspace[taget].h=headings[e.keyCode]);
+  //38 40 37 39 上下左右
+}
+
+
+//---------------------------------------
