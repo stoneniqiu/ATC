66b30de8f9751408d260a2e2856f25ec193465e7
stoneniqiu <stonzrj@163.com>
2018-01-21 17:16:19 +0800

    飞机相撞

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 var ctx,width=1200,height=600;
 var airspace = [];
+//空域中最大容量
 var max=10;
+//需指挥的飞机总数
+var taskTotal=20;
 var total=0,size=40,leave=0;
 //38 40 37 39 上下左右
 var headings={38:"up",40:"down",37:"left",39:"right"};
 var airspeed={0:"H",1:"M",2:"S"}
+var magntude={"H":3,"M":2,"S":1}
 var destination={0:"A",1:"B",2:"C",3:"D",4:"R"};
 //速度 H M S
 
@@ -22,6 +26,14 @@ function Plane(id,sx,sy,heading,url){
     this.move=movePlane
     this.speed=airspeed[getRandom(3)];
     this.D=destination[getRandom(5)];
+    this.state="move";
+    var slef=this;
+    this.getCenter=function(){
+        return{
+            x:slef.x+size/2,
+            y:slef.y+size/2
+        }
+    }
 }
 function getRandom(max,min){
     if(min&&min>=max){
@@ -82,7 +94,31 @@ function drawPlane(){
 
  //显示名字
  ctx.fillText(this.flightId+"_"+this.speed+"_"+this.D,this.x,this.y-5);
-
+ check(this);
+}
+//判断飞机是否相撞或离开
+function check(plane){
+  //计算中心点
+  var center=plane.getCenter();
+  //先与其他飞机判断
+  var otherplane=airspace.filter(n=>n.flightId!=plane.flightId&&n.flightId&&n.state!="crash");
+  otherplane.forEach(function(p){
+      var c1=p.getCenter();
+       var dx=Math.abs(center.x-c1.x);
+       var dy=Math.abs(center.y-c1.y);
+       if(plane.state!="crash"&&dx<size&&dy<size){
+           console.warn(plane.flightId+"与"+p.flightId+"相撞了");
+           info(plane.flightId+"与"+p.flightId+"相撞了");
+           p.state="crash";
+           plane.state="crash";
+           p.selected=false;
+           removePlane(plane.flightId);
+           removePlane(p.flightId);
+       }
+  })
+  //检查跑道
+  //检查入口
+  //得分
 }
 
 function movePlane(dx,dy){
@@ -91,10 +127,25 @@ function movePlane(dx,dy){
     //边界检查
     if(this.x<0||this.x>width||this.y<0||this.y>height){
         console.log(this.flightId+"离开管制空域");
-        airspace.splice(airspace.findIndex(item=>item.flightId==this.flightId),1)
-        leave++;
+        info(this.flightId+"离开管制空域");
+        removePlane(this.flightId);
         showPlaneNum();
     }
+}
+function removePlane(fid){
+    var index=airspace.findIndex(item=>item.flightId==fid);
+    airspace[index].selected=false;
+    airspace.splice(index,1)
+    leave++;
+}
+var ct;
+function info(str){
+ var span=document.getElementById("info");
+ span.innerHTML=str;
+ clearTimeout(ct);
+ ct=setTimeout(function(){
+  span.innerHTML="";
+ },5000)   
 }
 function Exit(name,x,y){
   this.name=name;
@@ -165,26 +216,28 @@ function refrash(){
     ctx.clearRect(0,0, width, height);
     for (var i = 0; i < airspace.length; i++) {
         var hit=airspace[i];
+        var key=magntude[hit.speed];
         switch(hit.h){
             case "up":
             hit.img="plane.png";
-            hit.move(0,-2);
+            hit.move(0,-key);
             break;
             case "down":
             hit.img="down.png";
-            hit.move(0,2)
+            hit.move(0,key)
             break;
             case "left":
             hit.img="left.png";
-            hit.move(-2,0)
+            hit.move(-key,0)
             break;
             case "right":
             hit.img="right.png";
-            hit.move(2,0)
+            hit.move(key,0)
             break;
         }
         hit.draw();
       }
+
 }
 
 function rotate(n,deg){
@@ -214,7 +267,7 @@ function tower(){
     getRandomPlane();
     getRandomPlane();
    setInterval(function(){
-    if(airspace.length<max){
+    if(airspace.length-5<max&&total<taskTotal){
         getRandomPlane();
     }
    },5000)
@@ -224,6 +277,20 @@ function showPlaneNum(){
     span.innerHTML=total;
     var le=document.getElementById("leave");
     le.innerHTML=leave;
+}
+
+function control(code){
+ console.log(code);
+ if(!taget) {
+     console.warn("还未选中飞机");
+    return;
+ }
+ if(code>2){
+     taget.h=headings[code];
+ }else{
+     taget.speed=airspeed[code];
+ }
+
 }
 
 function getRandomPlane(){
